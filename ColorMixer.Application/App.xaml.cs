fbdb76ef5dc5222d181d04e252b48f55e351cb70
RomanGrishin <roman1988grishin@gmail.com>
2023-08-26 22:14:03 +0300

    Set up dependency injection

--- ColorMixer.Application/App.xaml.cs
+++ ColorMixer.Application/App.xaml.cs
@@ -1,7 +1,28 @@
-﻿namespace ColorMixer.Application
+﻿using Microsoft.Extensions.DependencyInjection;
+using System.Windows;
+
+namespace ColorMixer.Application
 {
     public sealed partial class App : System.Windows.Application
     {
+        private readonly ServiceProvider _provider;
 
+        public App()
+        {
+            IServiceCollection services = new ServiceCollection();
+            ConfigureServices(services);
+            _provider = services.BuildServiceProvider();
+        }
+
+        private void ConfigureServices(IServiceCollection services)
+        {
+            services.AddSingleton<MainWindow>();
+        }
+
+        private void OnStartup(object sender, StartupEventArgs e)
+        {
+            MainWindow mainWindow = _provider.GetService<MainWindow>()!;
+            mainWindow.Show();
+        }
     }
 }
