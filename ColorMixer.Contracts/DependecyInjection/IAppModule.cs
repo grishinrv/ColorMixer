cc424c7187105e2ecb615750ca965854a33e9a6b
RomanGrishin <roman1988grishin@gmail.com>
2023-08-27 00:59:56 +0300

    Modules' services registration interface

---
+++ ColorMixer.Contracts/DependecyInjection/IAppModule.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace ColorMixer.Contracts.DependecyInjection
+{
+    /// <summary>
+    /// Composition node. 
+    /// </summary>
+    public interface IAppModule
+    {
+        /// <summary>
+        /// Registers implementations of internal services.
+        /// </summary>
+        /// <param name="services">Application services descriptors.</param>
+        public void ConfigureServices(IServiceCollection services);
+    }
+}
