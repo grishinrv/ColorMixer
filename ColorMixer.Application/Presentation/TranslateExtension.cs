5e099285763123ba6ff7c95a6941d27326815779
RomanGrishin <roman1988grishin@gmail.com>
2023-08-27 21:22:19 +0300

    Localization service + Translate markup extension

---
+++ ColorMixer.Application/Presentation/TranslateExtension.cs
@@ -0,0 +1,26 @@
+﻿using ColorMixer.Contracts.Services;
+using System;
+using System.Windows.Data;
+using System.Windows.Markup;
+
+namespace ColorMixer.Application.Presentation
+{
+    /// <summary>
+    /// Localizaing extension. XAML exapmle:
+    /// Text="{helpers:Translate Key=SomeKey}"
+    /// </summary>
+    public sealed class TranslateExtension : MarkupExtension
+    {
+        public string? Key { get; set; }
+
+        public override object ProvideValue(IServiceProvider serviceProvider)
+        {
+            return new Binding
+            {
+                Mode = BindingMode.OneWay,
+                Path = new System.Windows.PropertyPath($"[{Key}]"),
+                Source = LocalizationService.Instance
+            };
+        }
+    }
+}
---
+++ ColorMixer.Contracts/Services/LocalizationService.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel;
+using System.Globalization;
+using System.Reflection;
+using System.Resources;
+
+namespace ColorMixer.Contracts.Services
+{
+    public sealed class LocalizationService : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler? PropertyChanged;
+        private ResourceManager _resourceManager = new ResourceManager("ColorMixer.Contracts.Localization.Translation", Assembly.GetExecutingAssembly());
+
+        public static LocalizationService Instance { get; } = new LocalizationService();
+
+        public string this[string key] => _resourceManager.GetString(key, CultureInfo.CurrentUICulture) ?? string.Empty;
+
+        public void SetCulture(CultureInfo culture)
+        {
+            CultureInfo.CurrentUICulture = culture;
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(null));
+        }
+    }
+}
