50324a75e6f590e6e6918b2ab30d1e7c1666f3c8
RomanGrishin <roman1988grishin@gmail.com>
2023-08-27 21:24:52 +0300

    SettingsViewModel

--- ColorMixer.Application/ViewModels/SettingsViewModel.cs
+++ ColorMixer.Application/ViewModels/SettingsViewModel.cs
@@ -3,19 +3,24 @@ using ColorMixer.Contracts.Services;
 using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Threading.Tasks;
+using ColorMixer.Contracts.Localization;
+using ColorMixer.Application.Services;
 
 namespace ColorMixer.Application.ViewModels
 {
     public sealed partial class SettingsViewModel : ObservableObject
     {
         private readonly ISettingsRepository _settingsRepository;
+        private readonly ThemeSettingsManager _themeSettingsManager;
 
 #pragma warning disable CS8618
-        public SettingsViewModel(ISettingsRepository settingsRepository)
+        public SettingsViewModel(ISettingsRepository settingsRepository, ThemeSettingsManager themeSettingsManager)
 #pragma warning restore CS8618
         {
             _settingsRepository = settingsRepository;
+            _themeSettingsManager = themeSettingsManager;
         }
 
         public async Task OnFirstOpen()
@@ -52,23 +57,57 @@ namespace ColorMixer.Application.ViewModels
             "Violet", "Pink", "Magenta", "Crimson", "Amber", "Yellow", "Brown", "Olive", "Steel", "Mauve", "Taupe", "Sienna"
         };
 
+
+        public List<string> Languages { get; } = new List<string>()
+        {
+            "Русский язык", "Deutch", "English"
+        };
+
         /// <summary>
         /// Supported cultures: 
-        /// 
+        /// <see cref="Translation.Designer"/>
         /// </summary>
-        public List<string> Cultures { get; } = new List<string>()
+        private CultureInfo[] Cultures = new CultureInfo[]
+        { 
+            new CultureInfo("ru-Ru"),
+            new CultureInfo("de-De"),
+            new CultureInfo("en-Us"),
+        }; 
+
+        private int _selectedCultureIndex;
+
+        /// <summary>
+        /// Culture should be applied at once, othervise user will not be able to find proper button
+        /// </summary>
+        public int SelectedCultureIndex
         {
-            "ru", "de", "en"
-        };
+            get => _selectedCultureIndex;
+            set
+            {
+                SetProperty(ref _selectedCultureIndex, value);
+                LocalizationService.Instance.SetCulture(Cultures[value]);
+            }
+        }
 
         [RelayCommand]
         private async Task ApplyChanges()
         {
-            await _settingsRepository.SaveSettingIfChanged<string>(SettingsHelper.SELECTED_THEME, SelectedTheme);
-            await _settingsRepository.SaveSettingIfChanged<bool>(SettingsHelper.DARK_MODE, DarkMode);
-            await _settingsRepository.SaveSettingIfChanged<bool>(SettingsHelper.HIGHT_CONTRAST, HighContrast);
-            await _settingsRepository.SaveSettingIfChanged<bool>(SettingsHelper.USE_OS_THEME, UseOsTheme);
+            await ApplyThemeSettingsIfNeeded();
             await _settingsRepository.SaveSettingIfChanged<string>(SettingsHelper.SELECTED_UI_CULTURE, SelectedUiCulture);
+        }
+
+
+        private async Task ApplyThemeSettingsIfNeeded()
+        {
+            if (!await _settingsRepository.SaveSettingIfChanged<string>(SettingsHelper.SELECTED_THEME, SelectedTheme)
+                && !await _settingsRepository.SaveSettingIfChanged<bool>(SettingsHelper.DARK_MODE, DarkMode)
+                && !await _settingsRepository.SaveSettingIfChanged<bool>(SettingsHelper.HIGHT_CONTRAST, HighContrast)
+                && !await _settingsRepository.SaveSettingIfChanged<bool>(SettingsHelper.USE_OS_THEME, UseOsTheme))
+            {
+                return;
+            }
+
+            await _themeSettingsManager.ApplyCurrentThemeSettings();
         }
 
         [RelayCommand]
