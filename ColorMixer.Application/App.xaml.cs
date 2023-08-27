bc6d1b4c2234420b256f7d13ad9d2f874f5865df
RomanGrishin <roman1988grishin@gmail.com>
2023-08-27 14:41:28 +0300

    Settings manager + apply settings on startup

--- ColorMixer.Application/App.xaml.cs
+++ ColorMixer.Application/App.xaml.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using ColorMixer.Application.Services;
+using ColorMixer.Storage.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection;
 using System.Windows;
 
 namespace ColorMixer.Application
@@ -17,11 +19,18 @@ namespace ColorMixer.Application
         private void ConfigureServices(IServiceCollection services)
         {
             services.AddSingleton<MainWindow>();
+            services.AddSingleton<SettingsManager>();
+
+            new ColorMixerStorageModule().ConfigureServices(services);
         }
 
-        private void OnStartup(object sender, StartupEventArgs e)
+        private async void OnStartup(object sender, StartupEventArgs e)
         {
             MainWindow mainWindow = _provider.GetService<MainWindow>()!;
+            SettingsManager settingsManager = _provider.GetRequiredService<SettingsManager>();
+            await settingsManager.ApplyCurrentThemeSettings();
+     
+
             mainWindow.Show();
         }
     }
---
+++ ColorMixer.Application/Services/SettingsManager.cs
@@ -0,0 +1,37 @@
+﻿using ColorMixer.Contracts.Helpers;
+using ColorMixer.Contracts.Services;
+using ControlzEx.Theming;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ColorMixer.Application.Services
+{
+    public sealed class SettingsManager
+    {
+        private readonly ISettingsRepository _settingsRepository;
+        public SettingsManager(ISettingsRepository settingsRepository)
+        {
+            _settingsRepository = settingsRepository;
+        }
+
+        public async Task ApplyCurrentThemeSettings()
+        {
+            bool syncWithOs = await _settingsRepository.GetSetting<bool>(SettingsHelper.USE_OS_THEME);
+            if (syncWithOs)
+                ThemeManager.Current.ThemeSyncMode = ThemeSyncMode.SyncAll;
+            else
+            {
+                string themeName = await _settingsRepository.GetSetting<string>(SettingsHelper.SELECTED_THEME);
+                bool isDarkMode = await _settingsRepository.GetSetting<bool>(SettingsHelper.DARK_MODE);
+                bool isHighContrast = await _settingsRepository.GetSetting<bool>(SettingsHelper.HIGHT_CONTRAST);
+                themeName = isDarkMode ? $"{themeName}.Dark" : $"{themeName}.Light";
+                ThemeManager.Current.ChangeTheme(App.Current, themeName, isHighContrast);
+            }
+
+            ThemeManager.Current.SyncTheme();
+        }
+    }
+}
