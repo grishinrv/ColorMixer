f8e8194dc29c978235ba9eb05875a0dfc15fb89d
RomanGrishin <roman1988grishin@gmail.com>
2023-08-27 21:24:24 +0300

    Binding to localizable "settings" value

---
+++ ColorMixer.Application/Views/SettingsView.xaml.cs
@@ -0,0 +1,38 @@
+﻿using ColorMixer.Contracts.Services;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+
+namespace ColorMixer.Application.Views
+{
+    /// <summary>
+    /// Interaction logic for SettingsView.xaml
+    /// </summary>
+    public partial class SettingsView : UserControl
+    {
+        public SettingsView()
+        {
+            InitializeComponent();
+            Binding binding = new Binding("[settings]");
+            binding.Mode = BindingMode.OneWay;
+            binding.Source = LocalizationService.Instance;
+            BindingOperations.SetBinding(this, TitleProperty, binding);
+        }
+
+
+        public string Title
+        {
+            get { return (string)GetValue(TitleProperty); }
+            set { SetValue(TitleProperty, value); }
+        }
+
+        public static readonly DependencyProperty TitleProperty =
+            DependencyProperty.Register(
+                nameof(Title), 
+                typeof(string), 
+                typeof(SettingsView), 
+                new PropertyMetadata(string.Empty));
+
+    }
+
+}
