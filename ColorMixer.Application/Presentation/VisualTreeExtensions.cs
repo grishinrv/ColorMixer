576622e23b83dddc661eb00e9e0996ba6706d612
RomanGrishin <roman1988grishin@gmail.com>
2023-08-28 23:14:56 +0300

    VisualTreeExtensions

---
+++ ColorMixer.Application/Presentation/VisualTreeExtensions.cs
@@ -0,0 +1,66 @@
+﻿using System.Windows;
+using System.Windows.Media;
+
+namespace ColorMixer.Application.Presentation
+{
+    /// <summary>
+    /// Extension method for working with Visual Tree.
+    /// </summary>
+    public static class VisualTreeExtensions
+    {
+        /// <summary>
+        /// Get the closest Ancestor of specified type T of the given element.
+        /// </summary>
+        /// <typeparam name="T">Type of Ancestor to look for.</typeparam>
+        /// <param name="element">position in tree to start search.</param>
+        /// <returns></returns>
+        public static T? GetVisualParent<T>(this DependencyObject element) 
+            where T : DependencyObject
+        {
+            while (element != null && !(element is T))
+                element = VisualTreeHelper.GetParent(element);
+
+            return element as T;
+        }
+
+        /// <summary>
+        /// Get the closest Child of specified type T of the given parent.
+        /// </summary>
+        /// <typeparam name="T">Type of Child to look for.</typeparam>
+        /// <param name="parent">Position in tree to start search.</param>
+        /// <param name="point">Where to check.</param>
+        /// <returns></returns>
+        public static T? FindChildByTypeAndPoint<T>(this DependencyObject parent, Point point)
+            where T : DependencyObject
+        {
+            int childCount = VisualTreeHelper.GetChildrenCount(parent);
+            for (int i = 0; i < childCount; i++)
+            {
+                DependencyObject child = VisualTreeHelper.GetChild(parent, i);
+
+                if (child is T typedChild && IsPointInsideElement(typedChild, point))
+                {
+                    return typedChild;
+                }
+
+                DependencyObject? foundChild = FindChildByTypeAndPoint<T>(child, point);
+                if (foundChild is T result)
+                    return result;
+            }
+
+            return null;
+        }
+
+        private static bool IsPointInsideElement(DependencyObject element, Point point)
+        {
+            if (element is UIElement uiElement)
+            {
+                Point elementPoint = uiElement.TranslatePoint(new Point(0, 0), App.Current.MainWindow);
+                Rect elementBounds = new Rect(elementPoint, new Size(uiElement.RenderSize.Width, uiElement.RenderSize.Height));
+                return elementBounds.Contains(point);
+            }
+
+            return false;
+        }
+    }
+}
