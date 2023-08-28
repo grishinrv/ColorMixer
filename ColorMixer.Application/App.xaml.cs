07f2464a974cea55b0ea8c9308c3f7ebff75cf2b
RomanGrishin <roman1988grishin@gmail.com>
2023-08-28 10:17:24 +0300

    Registration for view models and view -> view model dependencies

--- ColorMixer.Application/App.xaml.cs
+++ ColorMixer.Application/App.xaml.cs
@@ -1,7 +1,14 @@
 ﻿using ColorMixer.Application.Services;
+using ColorMixer.Application.ViewModels;
 using ColorMixer.Storage.DependencyInjection;
+using CommunityToolkit.Mvvm.ComponentModel;
 using Microsoft.Extensions.DependencyInjection;
 using System.Windows;
+using ColorMixer.Application.Views;
+using System.Windows.Controls;
+using System;
+using ColorMixer.Contracts.Exceptions;
+using System.Windows.Threading;
 
 namespace ColorMixer.Application
 {
@@ -20,17 +27,40 @@ namespace ColorMixer.Application
         {
             services.AddSingleton<MainWindow>();
             services.AddSingleton<ThemeSettingsManager>();
+            services.AddSingleton<ViewModelResolver>(serviceProvider => view => 
+            {
+                ObservableObject viewModel;
+                switch (view)
+                {
+                    case SettingsView settingsView:
+                        viewModel = serviceProvider.GetRequiredService<SettingsViewModel>();
+                        break;
+                    case ColorMixerView colorMixerView:
+                        viewModel = serviceProvider.GetRequiredService<ColorMixerViewModel>();
+                        break;
+                    default: throw new FailedResolveInstanceException("ViewModel is not defined for pointed view (no match in delegate's implementation method)");
+                }
+                view.DataContext = viewModel;
+                return viewModel;
+            });
+            services.AddTransient<SettingsViewModel>();
+            services.AddTransient<ColorMixerViewModel>();
 
             new ColorMixerStorageModule().ConfigureServices(services);
         }
 
         private async void OnStartup(object sender, StartupEventArgs e)
         {
+            Dispatcher.UnhandledException += Dispatcher_UnhandledException;
             MainWindow mainWindow = _provider.GetService<MainWindow>()!;
             ThemeSettingsManager settingsManager = _provider.GetRequiredService<ThemeSettingsManager>();
             await settingsManager.ApplyCurrentThemeSettings();
 
             mainWindow.Show();
+        }
+
+        private void Dispatcher_UnhandledException(object sender, DispatcherUnhandledExceptionEventArgs e)
+        {
         }
     }
 }
