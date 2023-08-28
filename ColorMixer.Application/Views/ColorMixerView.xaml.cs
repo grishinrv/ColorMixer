9dd0c193ad194e4c1cc3d53ddab4bdeb59f65016
RomanGrishin <roman1988grishin@gmail.com>
2023-08-28 17:59:40 +0300

    Drag and drop (WIP)

--- ColorMixer.Application/Views/ColorMixerView.xaml.cs
+++ ColorMixer.Application/Views/ColorMixerView.xaml.cs
@@ -1,7 +1,12 @@
 ﻿using ColorMixer.Contracts.Services;
+using System;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using ColorMixer.Application.Controls;
 
 namespace ColorMixer.Application.Views
 {
@@ -10,6 +15,14 @@ namespace ColorMixer.Application.Views
     /// </summary>
     public partial class ColorMixerView : UserControl
     {
+        private bool _isDown;
+        private bool _isDragging;
+        private UIElement _originalElement;
+        private double _originalLeft;
+        private double _originalTop;
+        private CircleAdorner _overlayElement;
+        private Point _startPoint;
+
         public ColorMixerView()
         {
             InitializeComponent();
@@ -31,5 +44,126 @@ namespace ColorMixer.Application.Views
                 typeof(string),
                 typeof(ColorMixerView),
                 new PropertyMetadata("Defaul color set name"));
+
+
+        public void OnLoaded(object sender, RoutedEventArgs e)
+        {
+            TextBox tb = new TextBox { Text = "This is a TextBox. Drag and drop me" };
+            Canvas.SetTop(tb, 100);
+            Canvas.SetLeft(tb, 100);
+
+            MixingCanvas.Children.Add(tb);
+
+            MixingCanvas.PreviewMouseLeftButtonDown += MixingCanvas_PreviewMouseLeftButtonDown;
+            MixingCanvas.PreviewMouseMove += MixingCanvas_PreviewMouseMove;
+            MixingCanvas.PreviewMouseLeftButtonUp += MixingCanvas_PreviewMouseLeftButtonUp;
+            //MixingCanvas.DragOver += MixingCanvas_DragOver;
+            PreviewKeyDown += ColorMixer_PreviewKeyDown;
+            MixingColorPicker.SelectedColorChanged += MixingColorPicker_SelectedColorChanged;
+        }
+
+        private void MixingColorPicker_SelectedColorChanged(object sender, RoutedPropertyChangedEventArgs<Color?> e)
+        {
+            if (e.NewValue != null)
+            { 
+                
+            }
+        }
+
+        private void ColorMixer_PreviewKeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Escape && _isDragging)
+            {
+                DragFinished(true);
+            }
+        }
+
+        private void MixingCanvas_PreviewMouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+        {
+            if (_isDown)
+            {
+                DragFinished(false);
+                e.Handled = true;
+            }
+        }
+
+        private void DragFinished(bool cancelled)
+        {
+            Mouse.Capture(null);
+            if (_isDragging)
+            {
+                AdornerLayer.GetAdornerLayer(_overlayElement.AdornedElement).Remove(_overlayElement);
+
+                if (cancelled == false)
+                {
+                    Canvas.SetTop(_originalElement, _originalTop + _overlayElement.TopOffset);
+                    Canvas.SetLeft(_originalElement, _originalLeft + _overlayElement.LeftOffset);
+                }
+                _overlayElement = null;
+            }
+            _isDragging = false;
+            _isDown = false;
+        }
+
+        ///// <summary>
+        ///// Forbides to drop outside the canvas.
+        ///// </summary>
+        //private void MixingCanvas_DragOver(object sender, DragEventArgs e)
+        //{
+        //    if (e.Source != MixingCanvas)
+        //        e.Effects = DragDropEffects.None;
+        //}
+
+        private void MixingCanvas_PreviewMouseMove(object sender, MouseEventArgs e)
+        {
+            if (_isDown)
+            {
+                if ((_isDragging == false) &&
+                    ((Math.Abs(e.GetPosition(MixingCanvas).X - _startPoint.X) >
+                      SystemParameters.MinimumHorizontalDragDistance) ||
+                     (Math.Abs(e.GetPosition(MixingCanvas).Y - _startPoint.Y) >
+                      SystemParameters.MinimumVerticalDragDistance)))
+                {
+                    DragStarted();
+                }
+
+                if (_isDragging)
+                    DragMoved();
+            }
+        }
+
+        private void DragStarted()
+        {
+            _isDragging = true;
+            _originalLeft = Canvas.GetLeft(_originalElement);
+            _originalTop = Canvas.GetTop(_originalElement);
+            _overlayElement = new CircleAdorner(_originalElement);
+            AdornerLayer layer = AdornerLayer.GetAdornerLayer(_originalElement);
+            layer.Add(_overlayElement);
+        }
+
+        private void DragMoved()
+        {
+            Point currentPosition = Mouse.GetPosition(MixingCanvas);
+
+            _overlayElement.LeftOffset = currentPosition.X - _startPoint.X;
+            _overlayElement.TopOffset = currentPosition.Y - _startPoint.Y;
+        }
+
+        private void MixingCanvas_PreviewMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.Source == MixingCanvas)
+            {
+            }
+            else
+            {
+                _isDown = true;
+                _startPoint = e.GetPosition(MixingCanvas);
+                _originalElement = e.Source as UIElement;
+                MixingCanvas.CaptureMouse();
+                e.Handled = true;
+            }
+        }
+
     }
 }
