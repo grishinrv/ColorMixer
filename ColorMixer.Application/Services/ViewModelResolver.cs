a0ac6638ecbf02982f2f3c6b7ef3e3d7603c3204
RomanGrishin <roman1988grishin@gmail.com>
2023-08-28 10:16:40 +0300

    View model resolver delegate

---
+++ ColorMixer.Application/Services/ViewModelResolver.cs
@@ -0,0 +1,14 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using System.Windows.Controls;
+using ColorMixer.Contracts.Exceptions;
+
+namespace ColorMixer.Application.Services
+{
+    /// <summary>
+    /// Responsible for resolving viewModel for provided view and setting it's DataContext.
+    /// </summary>
+    /// <param name="view">A view instance.</param>
+    /// <returns>ViewModel</returns>
+    /// <throws><see cref="FailedResolveInstanceException"/></throws>
+    public delegate ObservableObject ViewModelResolver(Control view);
+}
