3061848ac1854b8989f002685186c78107de482a
RomanGrishin <roman1988grishin@gmail.com>
2023-08-28 10:16:35 +0300

    Exception for resolving errors

---
+++ ColorMixer.Contracts/Exceptions/FailedResolveInstanceException.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ColorMixer.Contracts.Exceptions
+{
+    /// <summary>
+    /// Imposiible to resolve required instance from DI containe, that means missing registration.
+    /// </summary>
+    public sealed class FailedResolveInstanceException : InvalidOperationException
+    {
+        public override string Message { get; }
+
+        public FailedResolveInstanceException(string message)
+        {
+            Message = message;
+        }
+    }
+}
