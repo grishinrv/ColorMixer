3a6650aa5c2b06ddbf20e9dcca8a264ac584d061
RomanGrishin <roman1988grishin@gmail.com>
2023-08-28 16:24:57 +0300

    ViewManager - for views open/close handling

---
+++ ColorMixer.Application/Services/IViewManager.cs
@@ -0,0 +1,16 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace ColorMixer.Application.Services
+{
+    /// <summary>
+    /// Managers current view, shown by Hamburger Menu.
+    /// </summary>
+    public interface IViewManager
+    {
+        /// <summary>
+        /// Closes current view.
+        /// </summary>
+        /// <param name="viewModel">View model, requesting close.</param>
+        void Close(ObservableObject viewModel);
+    }
+}
---
+++ ColorMixer.Application/Services/ViewManager.cs
@@ -0,0 +1,54 @@
+﻿using ColorMixer.Application.ViewModels;
+using CommunityToolkit.Mvvm.ComponentModel;
+using MahApps.Metro.Controls;
+using System;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace ColorMixer.Application.Services
+{
+    public sealed class ViewManager : IViewManager
+    {
+        private HamburgerMenuItem? _currentViewContainer;
+        private ObservableObject? _currentViewModel;
+        private ViewModelResolver _viewModelResolver;
+
+        public event Action<HamburgerMenuItem?> OnCurrentViewChanged;
+
+        public ViewManager(ViewModelResolver viewModelResolver)
+        {
+            _viewModelResolver = viewModelResolver;
+        }
+
+
+        public async Task SwitchViewRequested(HamburgerMenuItem viewContainer)
+        {
+            try
+            {
+                Control view = (Control)viewContainer.Tag;
+                _currentViewModel = _viewModelResolver.Invoke(view);
+                if (_currentViewModel is IViewModelInitializable initializable)
+                {
+                    await initializable.OnFirstOpen();
+                }
+                _currentViewContainer = viewContainer;
+                OnCurrentViewChanged?.Invoke(viewContainer);
+            }
+            catch (Exception ex)
+            {
+                // todo log
+                Environment.Exit(1);
+            }
+        }
+
+        public void Close(ObservableObject viewModel)
+        {
+            if (_currentViewModel == viewModel)
+            {
+                _currentViewModel = null;
+                _currentViewContainer = null;
+                OnCurrentViewChanged?.Invoke(_currentViewContainer);
+            }
+        }
+    }
+}
