f41a3e52e19ef7a0575f96c3e2545bae603d2017
RomanGrishin <roman1988grishin@gmail.com>
2023-08-28 10:15:51 +0300

    Support view model initialization call back

---
+++ ColorMixer.Application/ViewModels/IViewModelInitializable.cs
@@ -0,0 +1,12 @@
+﻿using System.Threading.Tasks;
+
+namespace ColorMixer.Application.ViewModels
+{
+    public interface IViewModelInitializable
+    {
+        /// <summary>
+        /// Init View Model.
+        /// </summary>
+        public Task OnFirstOpen();
+    }
+}
