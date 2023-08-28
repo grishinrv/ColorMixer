8482548021ace89965b287eca5ebeeb6e1bf01f7
RomanGrishin <roman1988grishin@gmail.com>
2023-08-28 10:18:04 +0300

    Color mixer view model (WIP)

---
+++ ColorMixer.Application/ViewModels/ColorMixerViewModel.cs
@@ -0,0 +1,13 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using System.Windows.Media;
+
+namespace ColorMixer.Application.ViewModels
+{
+    public sealed class ColorMixerViewModel : ObservableObject
+    {
+        public ColorMixerViewModel()
+        {
+            Color color = new Color();
+        }
+    }
+}
